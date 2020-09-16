14844e59ac1a37a23a10c6bcfad9266324681211
Ana Beatriz Freires Ferreira <30874488+anabeatrizfreires@users.noreply.github.com>
2020-09-16 09:23:32 -0300

    Update subprocesso.py

--- subprocesso.py
+++ subprocesso.py
@@ -3,6 +3,6 @@ import time
 
 while True:
 	result = subprocess.run(["python3", "data_hora_temperatura_umidade.py", "/dev/null"], stdout=subprocess.PIPE)
-	time.sleep(60 * 1)
+	time.sleep(60 * 5)
 	print("Reiniciando o código")
 	print(result)
