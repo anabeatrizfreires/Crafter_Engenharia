56a5bc61d85eb3126de4d33018c4781970bfe495
Ana Beatriz Freires Ferreira <30874488+anabeatrizfreires@users.noreply.github.com>
2020-10-14 14:57:05 -0300

    Update comissionamento_origem.py

--- comissionamento_origem.py
+++ comissionamento_origem.py
@@ -10,9 +10,9 @@ import os
 NUMERO_DE_COMISSIONAMENTOS = 19
 ENDERECO_INICIAL_MEDIDORES = 101
 ENDERECO_INICIAL_INVERSORES = 1
-vir = 1 #valor referencia corrente
-vfr = 230 #valor referencia tensao
-vpr = 1000 #valor referencia potencia
+VIR = 1 #valor referencia corrente
+VFR = 230 #valor referencia tensao
+VPR = 1000 #valor referencia potencia
 FORMULA_CONVERSAO_MEDIDORES = (medicao[0] * VR)/16384
 FORMULA_CONVERSAO_ENERGIA = medicao[0] * 10e9 * 1000 + medicao [0] * 10e3 + (medicao[0] /1000)
 
@@ -53,15 +53,15 @@ def collect_generico(tipo, comissionamento, i):
             medicao = medidor.read_float(registrador, 3, 2)
             
             if nome_medicao == 'tensao_AB' or 'tensao_BC' or 'tensao_CA':
-                  VR = vfr
+                  VR = VFR
                   medicao = FORMULA_CONVERSAO_MEDIDORES
 
              if nome_medicao == 'corrente_A' or 'corrente_B' or 'corrente_C' or 'corrente_neutro':
-                  VR = vir
+                  VR = VIR
                    medicao = FORMULA_CONVERSAO_MEDIDORES
 
               if nome_medicao == 'potencia_A' or 'potencia_B' or 'potencia_C' or 'potencia_total':
-                  VR = vpr
+                  VR = VPR
                    medicao = FORMULA_CONVERSAO_MEDIDORES
 
               if nome_medicao == 'fator_potencia_A' or 'fator_potencia_B' or 'fator_potencia_C' or 'fator_potencia_total':
