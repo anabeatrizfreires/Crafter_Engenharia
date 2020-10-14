562fadb93a584df830530e0fdaa83123ed42f070
Ana Beatriz Freires Ferreira <30874488+anabeatrizfreires@users.noreply.github.com>
2020-10-14 17:01:05 -0300

    Create enderecamento_supermercado_uniao.py

---
+++ enderecamento_supermercado_uniao.py
@@ -0,0 +1,34 @@
+  # ['4990 ~ 4999 - Número de série', 4990, 'STR16', 'ENUM'],
+  # ['5001 - Potência ativa nominal', 5001, 'U16', 'FIX1'], # 0,1kWh
+  # ['5003 - Rendimentos de potência diários', 5003, 'U16', 'FIX1'], # 0,1kWh
+  # ['5004 - Rendimento total de energia', 5004, 'U32', 'FIX0'], #kWh
+  # ['5006 - Tempo total de execução', 5006, 'U32', 'Duração'], # horas
+  # ['5008 - Temperatura interna', 5008, 'S16', 'FIX1'], #C
+  # ['5009 - Potência aparente total', 5009, 'U32', 'FIX0'], #VA
+  [ '5011 - MPPT 1 tensão' , 5011 , 'U16' , 'FIX1' ], # 0.1V
+  # ['5012 - MPPT 1 atual', 5012, 'U16', 'FIX1'], # 0.1A
+  [ '5013 - MPPT 2 voltagem' , 5013 , 'U16' , 'FIX1' ], # 0,1V
+  # ['5014 - MPPT 2 atual', 5014, 'U16', 'FIX1'], # 0.1A
+  # ['5015 - MPPT 3 tensão', 5015, 'U16', 'FIX1'], # 0,1V
+  # ['5016 - MPPT 3 atual', 5016, 'U16', 'FIX1'], # 0.1A
+  # ['5017 - Potência DC total', 5017, 'U32', 'FIX0'], #Watts
+  # ['5019 - Tensão da Fase A', 5019, 'U16', 'FIX1'], # 0.1V
+  # ['5020 - Tensão da fase B', 5020, 'U16', 'FIX1'], # 0,1V
+  # ['5021 - Tensão da Fase C', 5021, 'U16', 'FIX1'], # 0.1V
+  # ['5022 - Corrente da fase A', 5022, 'U16', 'FIX1'], # 0.1A
+  # ['5023 - Corrente da fase B', 5023, 'U16', 'FIX1'], # 0.1A
+  # ['5024 - Corrente da fase C', 5024, 'U16', 'FIX1'], # 0.1A
+  [ '5031 - Potência ativa total' , 5031 , 'U32' , 'FIX0' ], #Watts
+  # ['5033 - Potência reativa total', 5033, 'S32', 'FIX0'], #variância
+  # ['5035 - Fator de potência', 5035, 'S16', 'FIX1000'], # 0,001
+  # ['5036 - Frequência de grade', 5036, 'U16', 'FIX1'], # 0,1 Hz
+  # ['5049 - Potência reativa nominal', 5049, 'U16', 'FIX1'], #kilovariance
+  # ['5083 - Potência do medidor', 5083, 'S32', 'FIX'], #W
+  # ['5085 - Potência da fase do medidor A', 5085, 'S32', 'FIX'], #W
+  # ['5087 - Potência da fase do medidor B', 5087, 'S32', 'FIX'], #W
+  # ['5089 - Potência da fase C do medidor', 5089, 'S32', 'FIX'], #W
+  # ['5097 - Importação de energia diária', 5097, 'U32', 'FIX1'], # 0,1 kWh
+  # ['5113 - Tempo de execução diário', 5113, 'U16', 'Duração'], # minutos
+  # ['5144 - Rendimentos totais de potência', 5144, 'U32', 'FIX1'], # 0,1 kWh
+  # ['5146 - Tensão negativa para aterramento', 5146, 'S16', 'FIX1'], # 0,1V
+  [ '5148 - Frequência da rede' , 5148 , 'U16' , 'FIX2' ] # 0,01 Hz
